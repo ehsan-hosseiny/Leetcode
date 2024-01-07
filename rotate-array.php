40852d34b0057594d8936b0909e62c5457b6b812
pc <ehsanhossini@gmail.com>
2024-01-07 09:58:36 +0330

    added rotate array

---
+++ rotate-array.php
@@ -0,0 +1,44 @@
+<?php
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+/*
+Example 1:
+    Input: nums = [1,2,3,4,5,6,7], k = 3
+    Output: [5,6,7,1,2,3,4]
+    Explanation:
+    rotate 1 steps to the right: [7,1,2,3,4,5,6]
+    rotate 2 steps to the right: [6,7,1,2,3,4,5]
+    rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+    Input: nums = [-1,-100,3,99], k = 2
+    Output: [3,99,-1,-100]
+    Explanation: 
+    rotate 1 steps to the right: [99,-1,-100,3]
+    rotate 2 steps to the right: [3,99,-1,-100]
+
+*/
+
+class solution{
+    
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return NULL
+     */
+    function rotate(&$nums, $k) {
+        $size = count($nums);
+        $temp = $nums;
+        for($i =0;$i<$size;$i++){
+            $new_place = ($i + $k) % $size;
+
+            $nums[$new_place] = $temp[$i];
+        }
+        
+    }
+
+}
+
